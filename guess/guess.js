d2ef08202ffc49eca8ae315bd146b31cbeb5810b
AreebAhmed14 <areebfarooq549@gmail.com>
2024-04-03 00:36:39 +0500

    new

--- guess/guess.js
+++ guess/guess.js
@@ -1,8 +1,9 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-console.log("\nHey buddy, welcome to Areeb's number guessing game........");
-console.log("Note: you only have 10 chances for guessing the number......");
-console.log("let started....\n");
+import chalk from "chalk";
+console.log(chalk.blue("\nHey buddy, welcome to Jiya's number guessing game........"));
+console.log(chalk.yellow("Note: you only have 10 chances for guessing the number......"));
+console.log(chalk.green("let started....\n"));
 let random_number = Math.floor(Math.random() * 100 + 1);
 let machine = random_number;
 let chance = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th'];
@@ -10,32 +11,32 @@ for (let i = 0; i < chance.length; i++) {
     let input = await inquirer.prompt({
         name: "usernum",
         type: "number",
-        message: "guess number between 1-100...",
+        message: chalk.cyan("guess number between 1-100..."),
     });
     let human = input.usernum;
     // if(human:number){}
     if (human == machine) {
-        console.log("\ncongratulations this time you guessed right number...");
-        console.log("\ngame is over......\n");
+        console.log(chalk.magenta("\ncongratulations this time you guessed right number..."));
+        console.log(chalk.green("\ngame is over......\n"));
         break;
     }
-    else if (human == 9527) {
-        console.log("\nyou have been used cheat code in your " + chance[i] + " attempt: " + machine + "\n");
+    else if (human == "0709") {
+        console.log(chalk.yellow("\nyou have been used cheat code in your " + chance[i] + " attempt: " + machine + "\n"));
     }
     else {
         if (human < machine) {
-            console.log("\nOops! guessed number is wrong......");
-            console.log("hint: number is lesser.....");
-            console.log("you have been used your " + chance[i] + " attempt......\n");
+            console.log(chalk.blue("\nOops! guessed number is wrong......"));
+            console.log(chalk.green("hint: number is lesser....."));
+            console.log(chalk.red("you have been used your " + chance[i] + " attempt......\n"));
         }
         else if (human > machine) {
-            console.log("\nOops! guessed number is wrong......");
-            console.log("hint: number is higher.....");
-            console.log("you have been used your " + chance[i] + " attempt.....\n");
+            console.log(chalk.blue("\nOops! guessed number is wrong......"));
+            console.log(chalk.green("hint: number is higher....."));
+            console.log(chalk.red("you have been used your " + chance[i] + " attempt.....\n"));
         }
         else {
-            console.log("\ninput is not valid......");
-            console.log(`you have been loss your ${chance[i]} attempt by providing wrong input, please enter correct input. \n and if you have any query about this, read the instruction at the start of the game......\n`);
+            console.log(chalk.cyan("\ninput is not valid......"));
+            console.log(chalk.red(`you have been loss your ${chance[i]} attempt by providing wrong input, please enter correct input. \n and if you have any query about this, read the instruction at the start of the game......\n`));
         }
     }
 }
