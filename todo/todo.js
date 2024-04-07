5c77f2dfdcd616afe6860c1ae042dde02ad3fe77
AreebAhmed14 <areebfarooq549@gmail.com>
2024-04-08 01:23:01 +0500

    new

--- todo/todo.js
+++ todo/todo.js
@@ -1,64 +1,70 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-console.log("\nhey buddy! welcome to areeb's todo list.\n");
-let todo = [];
+import chalk from "chalk";
 let condition = true;
-while (condition) {
-    let ques = await inquirer.prompt({
-        name: "one",
-        type: "input",
-        message: "what you want to add..."
-    });
-    todo.push(ques.one);
-    let question = await inquirer.prompt({
-        name: "two",
-        type: "list",
-        choices: ["yes", "no"],
-        message: "do you want to add more...",
-    });
-    if (question.two === "no") {
-        condition = false;
-    }
-    else {
-        condition = true;
-    }
-}
-let questions = await inquirer.prompt({
-    name: "three",
-    type: "list",
-    choices: ["yes", "no"],
-    message: "would you like to check your list.....",
+let todo = [];
+console.log(chalk.blueBright("\nWelcome to Areeb's ToDo list...."));
+console.log(chalk.red("This program only for 18+ guy.....\n"));
+let age = await inquirer.prompt({
+    name: "falt",
+    type: "number",
+    message: chalk.cyan("Enter your age....")
 });
-if (questions.three === "yes") {
-    console.log("you are added these todos");
-    for (let i = 0; i < todo.length; i++) {
-        console.log(todo[i]);
-    }
-}
-else {
-    console.log("as your wish....");
-}
-let edit = await inquirer.prompt({
-    name: "delet",
-    type: "list",
-    choices: ["delet item", "exit"],
-    message: "what you want....."
-});
-if (edit.delet === "delet item") {
-    let select = await inquirer.prompt({
-        name: "todele",
-        type: "list",
-        choices: todo,
-        message: "select item you want to delet....."
-    });
-    for (let i = 0; i < todo.length; i++) {
-        if (select.todele == todo[i]) {
-            todo.splice(i, 1);
-            console.log("successfully Deleted");
+if (age.falt >= 18) {
+    console.log(chalk.greenBright("\nyou can use this.....\n"));
+    while (condition) {
+        let options = await inquirer.prompt({
+            name: "for",
+            type: "list",
+            choices: ["Add item", "check list", "delet item", "exit"],
+            message: chalk.magentaBright("what you want to do....."),
+        });
+        let user = options.for;
+        if (user === "Add item") {
+            let add = await inquirer.prompt({
+                name: "one",
+                message: chalk.yellowBright("What you want to add....."),
+            });
+            todo.push(add.one);
+            console.log(chalk.blue("successfully added.....\n"));
+        }
+        else if (user === "check list") {
+            if (todo == "") {
+                console.log(chalk.red("\nYour list is empty!\n"));
+            }
+            else {
+                console.log(chalk.green("you added these items....."));
+                for (let i = 0; i < todo.length; i++) {
+                    console.log(todo[i]);
+                }
+                console.log(chalk.cyan("wow! you make such a nice list...\n"));
+            }
+        }
+        else if (user === "delet item") {
+            if (todo == "") {
+                console.log(chalk.red("\nYour list is empty!\n"));
+            }
+            else {
+                let delet = await inquirer.prompt({
+                    name: "select",
+                    type: "list",
+                    choices: todo,
+                    message: chalk.blue("select item you want to delet....."),
+                });
+                for (let i = 0; i < todo.length; i++) {
+                    if (delet.select === todo[i]) {
+                        todo.splice(i, 1);
+                        console.log(chalk.redBright("delet successfully.....\n"));
+                    }
+                }
+            }
+        }
+        else {
+            console.log(chalk.greenBright("\nThanks for using Areeb's ToDo list......"));
+            condition = false;
         }
     }
 }
 else {
-    console.log("Thanks for using Areeb's todo list......");
+    console.log(chalk.red("\nsorry! you can't use this...."));
 }
-console.log(todo);
--- todo/todo.ts
+++ todo/todo.ts
@@ -1,65 +1,70 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-console.log("\nhey buddy! welcome to areeb's todo list.\n");
-let todo = [];
+import chalk from "chalk";
 let condition = true;
-while (condition) {
-    let ques = await inquirer.prompt({
-        name: "one",
-        type: "input",
-        message: "what you want to add..."
-    });
-    todo.push(ques.one);
-    let question = await inquirer.prompt({
-        name: "two",
-        type: "list",
-        choices: ["yes", "no"],
-        message: "do you want to add more...",
-    });
-    if (question.two === "no") {
-        condition = false;
-    }
-    else {
-        condition = true;
-    }
-}
-    let questions = await inquirer.prompt({
-    name: "three",
-    type: "list",
-    choices: ["yes", "no"],
-    message: "would you like to check your list.....",
+let todo:any = [];
+console.log(chalk.blueBright("\nWelcome to Areeb's ToDo list...."))
+console.log(chalk.red("This program only for 18+ guy.....\n"))
+let age = await inquirer.prompt({
+    name: "falt",
+    type:"number",
+    message:chalk.cyan("Enter your age....")
 })
-if (questions.three === "yes") {
-    console.log("you are added these todos");
-    for (let i = 0; i < todo.length; i++) {
-        console.log(todo[i]);
-    }
-}
-else{
-    console.log("as your wish....")
-}
-let edit = await inquirer.prompt({
-    name: "delet",
-    type: "list",
-    choices: ["delet item","exit"],
-    message:"what you want....."
+if(age.falt>=18){
+    console.log(chalk.greenBright("\nyou can use this.....\n"))
+while(condition){
+let options = await inquirer.prompt({
+    name:"for",
+    type:"list",
+    choices:["Add item","check list","delet item","exit"],
+    message:chalk.magentaBright("what you want to do....."),
 })
-if(edit.delet==="delet item"){
-    let  select = await inquirer.prompt({
-        name: "todele",
-        type: "list",
-        choices: todo,
-        message:"select item you want to delet....."
+let user = options.for;
+if(user==="Add item"){
+    let add = await inquirer.prompt({
+        name:"one",
+        message:chalk.yellowBright("What you want to add....."),
     })
-    for (let i = 0; i < todo.length; i++) {
-        if (select.todele == todo[i]) {
-            todo.splice(i,1)
-            console.log("successfully Deleted");
-        }
-
+    todo.push(add.one);
+    console.log(chalk.blue("successfully added.....\n"))
+}
+else if(user==="check list"){
+    if(todo==""){
+        console.log(chalk.red("\nYour list is empty!\n"))
+    }
+    else{
+    console.log(chalk.green("you added these items....."))
+    for(let i=0; i<todo.length; i++){
+        console.log(todo[i])
+    }
+    console.log(chalk.cyan("wow! you make such a nice list...\n"))
     }
 }
-else{
-    console.log("Thanks for using Areeb's todo list......");
+else if(user==="delet item"){
+    if(todo==""){
+        console.log(chalk.red("\nYour list is empty!\n"))
+    }
+    else{
+    let delet = await inquirer.prompt({
+        name:"select",
+        type:"list",
+        choices:todo,
+        message:chalk.blue("select item you want to delet....."),
+    })
+    for(let i=0; i<todo.length; i++){
+        if(delet.select===todo[i]){
+            todo.splice(i,1)
+            console.log(chalk.redBright("delet successfully.....\n"))
+        }
+    }
 }
-console.log(todo)
+}
+else{
+    console.log(chalk.greenBright("\nThanks for using Areeb's ToDo list......"))
+    condition=false
+}
+}
+}
+else{
+    console.log(chalk.red("\nsorry! you can't use this...."));
+}
