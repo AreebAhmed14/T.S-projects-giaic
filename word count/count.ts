7f656913410a5238bfcbdba8970a6da1bbed8f93
AreebAhmed14 <areebfarooq549@gmail.com>
2024-04-18 00:46:16 +0500

    new

---
+++ word count/count.ts
@@ -0,0 +1,26 @@
+import inquirer from "inquirer"
+let condition = true;
+console.log("hey buddy you are using very simple\n program in this you can determine the quantity\n of words , you write in your sentence....\n")
+while(condition){
+let uinput = await inquirer.prompt({
+    name:"word",
+    type:"input",
+    message:"Enter your sentence here..."
+})
+let word_count = uinput.word.trim().split(" ").length
+console.log(`your sentense contain ${word_count} words.`)
+console.log("")
+let uagain = await inquirer.prompt({
+    name:"yesorno",
+    type:"list",
+    choices:["Yes","No"],
+    message:"Do you want to still using this?"
+})
+if(uagain.yesorno==="No"){
+    condition = false;
+    console.log("\nThanks for using this.....\n")
+}
+else{
+    condition = true;
+}
+}
