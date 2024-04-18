70818c6572369af51501a6612563f25d4d1b5dc6
AreebAhmed14 <areebfarooq549@gmail.com>
2024-04-19 00:19:45 +0500

    new

--- word count/count.js
+++ word count/count.js
@@ -1,24 +1,25 @@
 import inquirer from "inquirer";
+import chalk from "chalk";
 let condition = true;
-console.log("hey buddy you are using very simple\n program in this you can determine the quantity\n of words , you write in your sentence....\n");
+console.log(chalk.magentaBright("\nhey buddy you are using word count program in this \nyou can determine the quantity of word in your sentence......\n"));
 while (condition) {
     let uinput = await inquirer.prompt({
         name: "word",
         type: "input",
-        message: "Enter your sentence here..."
+        message: chalk.cyan("Enter your sentence here...")
     });
     let word_count = uinput.word.trim().split(" ").length;
-    console.log(`your sentense contain ${word_count} words.`);
+    console.log(chalk.green(`\nyour sentense contain ${chalk.redBright(word_count)} words.`));
     console.log("");
     let uagain = await inquirer.prompt({
         name: "yesorno",
         type: "list",
         choices: ["Yes", "No"],
-        message: "Do you want to still using this?"
+        message: chalk.cyan("Do you want to still using this?")
     });
     if (uagain.yesorno === "No") {
         condition = false;
-        console.log("\nThanks for using this.....\n");
+        console.log(chalk.yellowBright("\nThanks for using this.....\n"));
     }
     else {
         condition = true;
